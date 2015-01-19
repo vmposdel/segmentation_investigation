bbec9653066539c20741c65f5926fd74048f48b5
vmposdel <vasilis1bos@gmail.com>
2015-01-19 05:22:37 +0200

    Added edge visualization

--- otsu_thresholding_experimental/otsu_thresholding_experimental.cpp
+++ otsu_thresholding_experimental/otsu_thresholding_experimental.cpp
@@ -7,22 +7,23 @@ int main( int argc, char** argv )
 {
     cv::Mat frame;
     cv::Mat otsuFrame; 
+    cv::Mat edgedFrame; 
     cv::Mat hist;
     int key = 1;
     initParams();
-    while(key != 'q')
-    {
-        captureFrame(frame);
-        const clock_t beginTime = clock();
-        if(frame.channels() == 3)
-            cv::cvtColor(frame, frame, CV_BGR2GRAY);
-        calcHistogram(frame, hist);
-        calcThresholded(frame, otsuFrame, hist);
-        double execTime = ( clock() - beginTime ) /  static_cast<double>(CLOCKS_PER_SEC );
-        printf("Time to execute: %f", execTime);
-        showImages(frame, otsuFrame);
-        key = cv::waitKey(1);
-    }
+    captureFrame(frame);
+    clock_t beginTime = clock();
+    if(frame.channels() == 3)
+        cv::cvtColor(frame, frame, CV_BGR2GRAY);
+    float sigma = 1;
+    cv::GaussianBlur(frame, frame, cv::Size(5,5), sigma, 0, BORDER_DEFAULT);
+    calcHistogram(frame, hist);
+    calcThresholded(frame, otsuFrame, hist);
+    double execTime = static_cast<double>(clock() - beginTime) /  static_cast<double>(CLOCKS_PER_SEC );
+    detectEdges(otsuFrame, edgedFrame);
+    printf("Time to execute: %f", execTime);
+    showImages(frame, otsuFrame, edgedFrame);
+    key = cv::waitKey(1);
 }
 
 void initParams()
@@ -32,13 +33,7 @@ void initParams()
 
 void captureFrame(cv::Mat& frame)
 {
-    cv::VideoCapture camera(cameraId);
-    cv::Mat destFrame;
-
-     // Get the next frame.
-    camera.grab();
-    camera.retrieve(destFrame);
-    frame = destFrame;
+    frame = cv::imread("/home/v/Documents/PANDORA/Vision/Image Segmentation/BSDS300/images/train/189011.jpg");
 }
 
 void calcHistogram(cv::Mat& frame,  cv::Mat& histNorm)
@@ -59,10 +54,25 @@ void calcThresholded(cv::Mat& frame, cv::Mat& otsuFrame, cv::Mat& hist)
    cv::threshold(frame, otsuFrame, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
 }
 
-void showImages(cv::Mat& frame, cv::Mat& otsuFrame)
+void detectEdges(cv::Mat& frame, cv::Mat& edgedFrame)
+{
+  cv::Mat dstImg;
+  cv::Mat dst;
+  int thresholdLow = 10;
+  int ratio = 3;
+  float sigma = 1;
+  cv::GaussianBlur(frame, dstImg, cv::Size(5,5), sigma, 0, BORDER_DEFAULT);
+  cv::Canny(dstImg, dstImg, thresholdLow, ratio * thresholdLow);
+  dstImg.copyTo(edgedFrame);
+}
+
+void showImages(cv::Mat& frame, cv::Mat& otsuFrame, cv::Mat& edgedFrame)
 {
     cv::namedWindow("OriginalImage", CV_WINDOW_AUTOSIZE);
     cv::namedWindow("OtsuThresholded", CV_WINDOW_AUTOSIZE);
+    cv::namedWindow("Edges", CV_WINDOW_AUTOSIZE);
     cv::imshow("OriginalImage", frame);
     cv::imshow("OtsuThresholded", otsuFrame);
+    cv::imshow("Edges", edgedFrame);
+    cv::waitKey(100000);
 }
