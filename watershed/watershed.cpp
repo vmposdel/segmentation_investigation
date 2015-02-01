559a8b4e12898eca7320c8ee8c1b7984bde1cfcd
vmposdel <vasilis1bos@gmail.com>
2015-02-01 18:05:14 +0200

    Added dataset evaluation and time calculation

--- watershed/watershed.cpp
+++ watershed/watershed.cpp
@@ -1,14 +1,16 @@
 #include "opencv2/opencv.hpp"
 #include <string>
 #include <stdio.h>
+#include "include/tinydir.h"
+#include <sys/time.h>
 
 using namespace cv;
 using namespace std;
 
 class WatershedSegmenter{
-private:
+  private:
     cv::Mat markers;
-public:
+  public:
     void setMarkers(cv::Mat& markerImage)
     {
       markerImage.convertTo(markers, CV_32S);
@@ -22,22 +24,31 @@ public:
     }
 };
 
+static void captureFrame(std::vector<cv::Mat>& inImages, std::vector<std::string>& imageNames);
+
 /*Disable imshows,saves the segmented images.
  * Need to save inputs as 0-10 in the end
  */
 int main(int argc, char* argv[])
 {
-  char name[10];
-  int i=0;
-  while(1)
+  cv::Mat image;
+  std::vector<cv::Mat> inImages(202);
+  std::stringstream imgName;
+  std::vector<std::string> imageNames;
+  struct timeval startwtime, endwtime;
+  double execTime;
+  imageNames.clear();
+  int i = 1;
+  gettimeofday (&startwtime, NULL);
+  captureFrame(inImages, imageNames);
+  for( int i = 0; i < imageNames.size(); i++ ) 
   {
-    sprintf(name, "img%d.jpeg",i);
-    cv::Mat image = cv::imread(name,1);
-    
+    inImages[i].copyTo(image);
+
     if(!image.data)
     {
       cout<<"error while reading image\n";
-      break;
+      continue;
     }
     cv::Mat binary;
     cv::cvtColor(image, binary, CV_BGR2GRAY);
@@ -69,11 +80,40 @@ int main(int argc, char* argv[])
     cv::Mat result = segmenter.process(image);
     result.convertTo(result,CV_8U);
     //imshow("final_result", result);
-    sprintf(name,"waterResult%d.jpeg",i);
-    imwrite(name,result);
-    i++;
+    //cv::waitKey(20000);
+    //sprintf(name,"waterResult%d.jpeg",i);
+    //imwrite(name,result);
+    imgName << "../../../dataset_WatershedMarkers/" << imageNames.at(i);
+    cv::imwrite( imgName.str(), markers );
+    imgName.str("");
   }
+  gettimeofday (&endwtime, NULL);
+  execTime = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+      + endwtime.tv_sec - startwtime.tv_sec);
+  printf("Time to execute: %f\n", execTime);
   cv::waitKey(0);
 
   return 0;
+}
+
+static void captureFrame(std::vector<cv::Mat>& inImages, std::vector<std::string>& imageNames)
+{
+  tinydir_dir dir;
+  tinydir_open(&dir, "/home/v/Documents/Pandora_Vision/opencv_traincascade/new_svm_data/data/Test_Negative_Images");
+  std::stringstream imgName;
+  int i = 0;
+  while (dir.has_next)
+  {
+    tinydir_file file;
+    tinydir_readfile(&dir, &file);
+    if(strcmp(file.name, ".") != 0 && strcmp(file.name, "..") != 0)
+    {
+      imageNames.push_back(file.name);
+      imgName << "/home/v/Documents/Pandora_Vision/opencv_traincascade/new_svm_data/data/Test_Negative_Images/" << file.name;
+      inImages[i] = cv::imread(imgName.str());
+      imgName.str("");
+    }
+    tinydir_next(&dir);
+    i++;
+  }
 }
