a11fcd0c79064ec95486fec1b59d4bc1b77d4617
vmposdel <vasilis1bos@gmail.com>
2015-02-24 13:10:40 +0200

    Added centers of contour extraction, maybe will be used as seeds

--- region_growing_experimental/standard_deviation_example/std_example.cpp
+++ region_growing_experimental/standard_deviation_example/std_example.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include<opencv2/opencv.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
 #include <sys/time.h>
 
 using namespace std;
 using namespace cv;
 
 Mat sigma;
+vector<vector<Point> > contours;
+vector<Vec4i> hierarchy;
 int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
@@ -27,8 +30,6 @@ void thresh_callback(int, void*)
 {
     Mat canny_output;
     cv::Mat tempSigma;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
     cv::GaussianBlur(sigma, tempSigma, cv::Size(0, 0), gaussiansharpenblur);
 
     /// Detect edges using canny
@@ -36,7 +37,14 @@ void thresh_callback(int, void*)
     cv::Mat structuringElement = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(40, 40));
     cv::morphologyEx( canny_output, canny_output, cv::MORPH_CLOSE, structuringElement );
     /// Find contours
-    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    findContours( canny_output, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    vector<Moments> mu(contours.size() );
+    vector<Point2f> mc( contours.size() );
+    //Mass center
+    for( int i = 0; i < contours.size(); i++ ){
+        mu[i] = moments( contours[i], false );
+        mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); 
+    }
 
     if(debugShow)
     {
@@ -46,6 +54,12 @@ void thresh_callback(int, void*)
         {
             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
             drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+            cout << "contour: " << i << ", size: " << contours[i].size() << "\n";
+        }
+
+        //Mass center
+        for( int i = 0; i < contours.size(); i++ ){ 
+            cout << "Mass center: " << (int)mc[i].x << ", " << (int)mc[i].y << "\n";
         }
 
         /// Show in a window
@@ -55,13 +69,32 @@ void thresh_callback(int, void*)
     }
 }
 
+cv::Mat& subtractBackground(cv::Mat& image)
+{
+    cv::Mat foreground;
+    cv::Mat background;
+    cv::BackgroundSubtractorMOG2 bg;
+    //bg = Algorithm::create(“BackgroundSubtractor.MOG2″);
+    //bg.set(“nmixtures”,3);
+    bg.operator ()(image, foreground);
+    bg.getBackgroundImage(background);
+    //bg.nmixtures = 3;
+    //bg.bShadowDetection = false;
+    cv::erode(foreground, foreground, cv::Mat());
+    cv::dilate(foreground, foreground, cv::Mat());
+    cv::imshow("Foreground", foreground);
+    cv::imshow("Background", background);
+    cv::waitKey();
+    return foreground;
+}
+
 int main()
 {
     struct timeval startwtime, endwtime;
     double seq_time;
     gettimeofday (&startwtime, NULL);
 
-    Mat image = imread("../../negative43.jpg", 0);//cv::IMREAD_ANYDEPTH|cv::IMREAD_ANYCOLOR);
+    Mat image = imread("../../negative21.jpg");//cv::IMREAD_ANYDEPTH|cv::IMREAD_ANYCOLOR);
     if(!image.data)
     {
         cout << "Cannot open image \n";
@@ -73,6 +106,7 @@ int main()
     //for ( int i = 1; i < maxKernelLength; i = i + 2 )
     //    medianBlur (image, image, i);
     //image.copyTo(tempMedian);
+    //image = subtractBackground(image);
 
     Mat image32f;
     image.convertTo(image32f, CV_32F);
